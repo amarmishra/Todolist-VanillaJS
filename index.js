05f9313716978f4a9acf8175bd0150575fb16394
amarmishra <amar.m130@gmail.com>
2023-04-10 14:16:39 +0530

    converted to nodejs server

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express=require('express')
+
+const app=express()
+
+
+app.use(express.static('./assets'))
+app.set('view engine','ejs')
+app.set('views','./views')
+
+
+
+//parse req urlencoded data and json  
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json({limit:'1mb'}))
+
+
+
+app.get('/',(req,res)=>{
+   return res.render('index')
+})
+
+app.listen(process.env.EXPRESS_SERVER_PORT_NO,(err)=>{
+    if(err){return console.log(`Error:${err}`)}
+    console.log(`EXPRESS Server running successfully on port${process.env.EXPRESS_SERVER_PORT_NO}`)
+})
